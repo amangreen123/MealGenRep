abbd00d5e917e351db1085cccfdab9e4f2f6ce26
amangreen123 <Aaron.green1@marist.edu>
2025-02-16 19:05:04 -0500

    Api Ill be using to create additonal recipes

---
+++ MealGenerator/my-react-app/src/getTheMealDB.jsx
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+//Filter by Ingredients
+//www.themealdb.com/api/json/v1/1/filter.php?i=
+
+//Filter by Category
+//https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegan
+
+//Give the details of the recipe
+//www.themealdb.com/api/json/v1/1/lookup.php?i=52772
+
