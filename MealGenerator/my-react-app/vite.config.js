70d4a2ed924e1fb73ebf4b89556b358338574e76
amangreen123 <Aaron.green1@marist.edu>
2025-03-30 17:11:07 -0400

    added extenstion to the vite build

--- MealGenerator/my-react-app/vite.config.js
+++ MealGenerator/my-react-app/vite.config.js
@@ -9,7 +9,8 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@': path.resolve(__dirname, './src'),
+      extensions: ['.js', '.jsx', '.ts', '.tsx']
     },
   },
   build:{
